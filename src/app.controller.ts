04a6b6fe0a9dfcb847f75125678422a34f780e88
Ramazan Chasygov <theliketurbo@gmail.com>
2022-07-02 15:04:13 +0300

    feat: set up create game route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateGameDto } from './dto/create-game.dto';
+import { Game, GameDocument } from './schemas/game.schema';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@InjectModel(Game.name) private gameModel: Model<GameDocument>) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  games(): Promise<Game[]> {
+    return this.gameModel.find().exec();
+  }
+
+  @Post()
+  createGame(@Body() createGameDto: CreateGameDto): Promise<Game> {
+    const createdGame = new this.gameModel(createGameDto);
+    return createdGame.save();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Game, GameSchema } from './schemas/game.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/poker'),
+    MongooseModule.forFeature([{ name: Game.name, schema: GameSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
