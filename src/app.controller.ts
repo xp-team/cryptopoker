876657a6c1840ed8022ace4755c80130da440fa1
Ramazan Chasygov <theliketurbo@gmail.com>
2022-07-02 15:06:05 +0300

    chore: add eslint sort plugin

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+
 import { CreateGameDto } from './dto/create-game.dto';
 import { Game, GameDocument } from './schemas/game.schema';
 
